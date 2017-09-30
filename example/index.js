d843ddfaf527c25bc11f9e21bf7f0523cbd61aa1
minemidnight <minemidnightmc@gmail.com>
2017-09-30 11:28:10 -0400

    Create library base

---
+++ example/index.js
@@ -0,0 +1,43 @@
+const wiggle = require("../index");
+const client = wiggle();
+client.set("owner", "155112606661607425");
+client.set("prefixes", ["mention", "!"]);
+client.set("token", "bot token");
+client.use("message", wiggle.middleware.commandParser());
+
+client.use("ping", (message, next) => {
+	console.log("Ping middleware");
+
+	next();
+});
+client.command("ping", { sendTyping: true, replyResult: true }, context => {
+	console.log("Ping handler");
+	return "Pong!";
+});
+
+const creator = wiggle.Category("creator"); // eslint-disable-line new-cap
+creator.use((message, next) => {
+	console.log("Creator middleware");
+	if(message.author.id === client.get("owner")) next();
+	else message.channel.createMessage("You cant use this! Its creator only");
+});
+
+const util = require("util");
+creator.command("eval", { replyResult: true, caseSensitive: true }, async context => {
+	const { message } = context;
+	let guild = message.channel.guild, channel = message.channel, author = message.author, member = message.member; // eslint-disable-line
+
+	try {
+		let output = await eval(`(async function(){${message.content.replace(/“|”/g, "\"")}}).call()`);
+		output = util.inspect(output, { depth: 0 }).substring(0, 1900);
+		return `:white_check_mark: **Output:** \`\`\`js\n${output}\n\`\`\``;
+	} catch(error) {
+		return `:x: **Error:** \`\`\`\n${error}\n\`\`\``;
+	}
+});
+
+client.use(creator);
+
+client.connect();
+
+process.on("unhandledRejection", err => console.error(err.stack));
---
+++ middleware/commandParser.js
@@ -0,0 +1,44 @@
+const commandParser = async (message, next, wiggle) => {
+	let prefixes;
+	if(wiggle._options.getPrefixes) prefixes = await wiggle._options.getPrefixes(message);
+	else prefixes = wiggle._options.prefixes || ["mention"];
+
+	prefixes = prefixes.filter((ele, i, arr) => arr.indexOf(ele) === i);
+	if(~prefixes.indexOf("mention")) prefixes[prefixes.indexOf("mention")] = `<@!?${wiggle._client.user.id}>`;
+	const prefixRegex = new RegExp(`^(?:${prefixes.join("|")}),?(?:\\s+)?([\\s\\S]+)`, "i");
+
+	message.originalContent = message.content;
+	let match = message.content.match(prefixRegex);
+	if(!match && message.channel.guild) return;
+	else if(match) [, message.content] = match;
+
+	let command;
+	if(!~message.content.indexOf(" ")) {
+		command = message.content;
+		message.content = "";
+	} else {
+		command = message.content.substring(0, message.content.indexOf(" "));
+		message.content = message.content.substring(message.content.indexOf(" "));
+	}
+	command = command.toLowerCase().trim();
+
+	const middlewares = wiggle._middleware.reduce((total, mid) => {
+		if(mid.type === "category") {
+			const commands = [...mid.category._middleware].filter(mid2 => mid2.type === "command");
+			total = total.concat(commands);
+		} else if(mid.type === "command") {
+			total.push(mid);
+		}
+
+		return total;
+	}, []);
+
+	command = middlewares.find(middleware => middleware.name === command || ~middleware.command.aliases.indexOf(command));
+	if(!command) return;
+	else if(!command.command.options.caseSensitive) message.content = message.content.toLowerCase();
+	message.command = command;
+
+	next();
+};
+
+module.exports = () => commandParser;
