9992c20c52968b1d7db2a8c4f026b5dc0de1c14c
minemidnight <minemidnightmc@gmail.com>
2017-10-29 01:03:55 -0400

    Add locale imports and prefixes

--- example/commands/default/say.js
+++ example/commands/default/say.js
@@ -1,4 +1,4 @@
 module.exports = {
-	run: ({ message }) => message.t("say.success", { word: message.args[1] || message.t("words.nothing") }),
+	run: ({ message }) => message.t("say.success", { word: message.args[0] || message.t("words.nothing") }),
 	args: [{ type: "text", label: "word", optional: true }]
 };
