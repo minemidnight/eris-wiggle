b7f44bd0352c2ca5d722c4a49cd6dc21b7250596
minemidnight <minemidnightmc@gmail.com>
2017-12-30 18:32:26 -0500

    Fixes issue addressed in #8 but better

--- middleware/dbots.js
+++ middleware/dbots.js
@@ -3,7 +3,7 @@ const post = async (guild, next, wiggle) => {
 	await superagent.post(`https://bots.discord.pw/api/bots/${wiggle.erisClient.user.id}/stats`)
 		.set("Authorization", siteKey)
 		.send({
-			server_count: guild.shard.guildCount, // eslint-disable-line camelcase
+			server_count: guild.shard.client.guilds.filter(({ shard: { id } }) => id === guild.shard.id).length, // eslint-disable-line camelcase
 			shard_count: guild.shard.client.options.maxShards, // eslint-disable-line camelcase
 			shard_id: guild.shard.id // eslint-disable-line camelcase
 		})
